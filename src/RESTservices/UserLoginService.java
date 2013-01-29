9f719253eb8ee9019e6a04f65c294103fadb92df
Alex Martin <a.martin.12@ucl.ac.uk>
2013-01-29 00:14:39 +0000

    Lots of work on authentication.
    Added paypal.html for Cyrus.
    
    
    
    Signed-off-by: Alex Martin <a.martin.12@ucl.ac.uk>

---
+++ src/RESTservices/UserLoginService.java
@@ -0,0 +1,34 @@
+package RESTservices;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+import RESTdataEntities.UserLoginEntity;
+@Path("/UserLoginService")
+public class UserLoginService {
+	
+	@GET
+	@Path("/Username/{Username}")
+	@Produces({MediaType.APPLICATION_XML})
+	public UserLoginEntity loginXML(@PathParam("Username") String Username) {
+		
+		RESTdataEntities.UserLoginEntity UserResult = new UserLoginEntity();
+		org.hibernate.Session HibernateSession = hibernateEntities.HibernateUtil.getSessionFactory().openSession();
+		try{
+			org.hibernate.Query QueryResult = HibernateSession.createQuery("FROM Users");
+			java.util.List users = QueryResult.list();
+	        for (int i = 0; i < users.size(); i++) {
+	        	hibernateEntities.User currentUser = (hibernateEntities.User) users.get(i);
+	        	UserResult.setUsername(currentUser.getUserName());
+	        	UserResult.setPassword(currentUser.getUserPassword());
+	        	UserResult.setSalt(currentUser.getSalt());
+	        }
+	      }catch (Exception e) {
+	         e.printStackTrace(); 
+	      }finally {
+	    	  HibernateSession.close(); 
+	      }
+		
+		return UserResult;
+	}
+}
