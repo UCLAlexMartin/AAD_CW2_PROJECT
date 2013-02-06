df593f077dfae48ed15580521b82afae9e9359fd
Alex Martin <a.martin.12@ucl.ac.uk>
2013-02-06 22:29:37 +0000

    Updates from Basilis.

--- src/RESTservices/UserService.java
+++ src/RESTservices/UserService.java
@@ -3,7 +3,10 @@ package RESTservices;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
+import hibernateEntities.Form;
+import hibernateEntities.FormFields;
 import hibernateEntities.User;
 import hibernateManagers.UserManager;
 
